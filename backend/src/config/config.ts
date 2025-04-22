4e24ae0d9502c2d69d3dfbc552a7118ae19dfdb9
RosarioB <borgesiros@gmail.com>
2025-04-22 11:27:45 +0200

    Removed unused env property from config

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 
 const requiredEnvVariables = [
-  { key: "PRIVATE_KEY", message: "PRIVATE_KEY is empty" },
   { key: "NEYNAR_API_KEY", message: "Make sure you set NEYNAR_API_KEY in your .env file" },
   { key: "SIGNER_UUID", message: "Make sure you set SIGNER_UUID in your .env file" },
   { key: "MONGO_URL", message: "Make sure you set MONGO_URL in your .env file" },
@@ -16,7 +15,6 @@ requiredEnvVariables.forEach(({ key, message }) => {
 export const config = {
   mongodb_url: process.env.MONGO_URL,
   port: process.env.PORT || 3001,
-  privateKey: process.env.PRIVATE_KEY,
   neynar_api_key: process.env.NEYNAR_API_KEY,
   signer_uuid: process.env.SIGNER_UUID,
   eliza_url: process.env.ELIZA_URL || "http://localhost:3000",
